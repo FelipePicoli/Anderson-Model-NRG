0fa97fe826bcfe55ea13618613de5c8ddc3368e5
FelipePicoli <felipepicoli@usp.br>
2024-08-01 08:46:55 +0200

    v11

--- SIAM_ex.jl
+++ SIAM_ex.jl
@@ -16,10 +16,11 @@ using LaTeXStrings  # For LaTex code
 #############################################--- Setting the parameters ---################################################
 ############################################################################################################################
 
-V=0.1                               # Hybridization
-U=2.0                               # Repulsion
-E_d=U/2                            # Impurity Energy
+V=0.1                              # Hybridization
+U=5.0                               # Repulsion
+E_d=-U/2                            # Impurity Energy
 L=5                                 # Bumber of Bath sites
+Occ_number=false                     # Do you want calculate the Occupation number?
 
 ############################################################################################################################
 #################################################---- Functions ----########################################################
@@ -30,10 +31,13 @@ L=5                                 # Bumber of Bath sites
 
 function Basis(L)
 
-    local_states=["vac","up","down","updown"]
+    #local_states=["vac","up","down","updown"]
 
-    States=[["vac"],["up"],["down"],["updown"]]
+    #States=[["vac"],["up"],["down"],["updown"]]
 
+    local_states=[0,1,2]
+
+    States=[[0],[1],[2]]
 
 
     for l=2:1:L
@@ -44,7 +48,7 @@ function Basis(L)
 
             vec=copy(st)
 
-            for j=1:4
+            for j=1:1:length(local_states)
 
                 push!(sup,push!(vec,local_states[j]))
                 vec=copy(st)
@@ -67,15 +71,15 @@ function  Diagonal_mH(State,E_d,U)
 
     local_state=State[1]
 
-    if local_state=="vac"
+    if local_state==0
 
         return 0.0
 
-    elseif local_state=="up" || local_state=="down"
+    elseif local_state==1 
 
         return E_d
 
-    else
+    elseif local_state==2
 
         return 2.0*E_d+U
 
@@ -86,20 +90,32 @@ end
 function  off_diagonal_mH(State1,State2,V)
 
 
-    dif=findall(in(State1),State2) # Find where the vectors are iqual
+    dif=findall(State1.!=State2) # Find where the vectors are iqual
 
-    if abs(length(State1)-length(dif))==2
+    if length(dif)==2
 
-        if State1[1]==State2[1] && State1[2]==State2[2]
+        if abs(dif[1]-dif[2])==1 
 
-            return -1.0
+            if ((State1[dif[1]]+State1[dif[2]])==(State2[dif[1]]+State2[dif[2]])) && abs(State1[dif[1]]-State2[dif[1]])==1 && abs(State1[dif[2]]-State2[dif[2]])==1
+
+                if dif[1]==1 || dif[2]==1
+
+                    return sqrt(2)*V
+                else
+                    return -1.0
+
+                end
+
+            else
+                
+                return 0.0
+            end
 
         else
-
-            return sqrt(2)*V
-
+            return 0.0
         end
 
+          
     else
 
         return 0.0
@@ -134,20 +150,63 @@ function mH(Basis_states,E_d,U,V)
     return mat
 end
 
+###########################################---- Impurity Occupation Number ----############################################
+
+function n_f(Vec,Basis_states)
+
+    n=0.0
+
+    for i=1:1:length(Vec)
+
+        state=Basis_states[i]
+
+        if state[1]==0
+
+            N_el=0.0
+
+        elseif state[1]==1 
+
+           # N_el=1.0
+
+            N_el=2.0
+
+        elseif state[1]==2
+
+            N_el=2.0
+
+        end
+
+        n+=abs2(Vec[i])*N_el
+
+    end
+
+    return n
+end
 
 ############################################################################################################################
 #########################################---- Main Calculation function ----################################################
 ############################################################################################################################
 
-function SIAM_ex(L,E_d,U,V)
+function SIAM_ex(L,E_d,U,V,Occ_number)
 
     Basis_states=Basis(L)
 
     H=mH(Basis_states,E_d,U,V)
 
-    E0=eigmin(H)
+    if Occ_number
 
-    return E0, H
+        E,Vec=eigen(H)
+
+        N_imp=n_f(Vec[:,1],Basis_states)
+
+        return E[1],N_imp, H
+
+    else
+
+        E=eigmin(H)
+
+        return E, H
+    end
 
 end
 
@@ -157,4 +216,8 @@ end
 #################################################---- Really Calculation ----###############################################
 ############################################################################################################################
 
-@time e0,h=SIAM_ex(L,E_d,U,V)
+if Occ_number
+    @time e0,nf,H=SIAM_ex(L,E_d,U,V,Occ_number)
+else
+    @time e0,H=SIAM_ex(L,E_d,U,V,Occ_number)
+end
